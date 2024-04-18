7b9f6fddd386305d97d6ee9c147218097aa6c378
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-18 18:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let firt = document.querySelector(".fname");
+	let lat = document.querySelector(".lname");
+	let firstname = firt.value;
+	let lastname = lat.value;
+	alert(firstname +" "+lastname);
 }
